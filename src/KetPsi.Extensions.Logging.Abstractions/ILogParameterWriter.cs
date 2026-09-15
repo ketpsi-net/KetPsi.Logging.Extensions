2cd32b612f70db0e603d7a4ab31247a30c031a41
MohamadArsaln Imamverdi <emamverdi@utravs.com>
2026-09-16 00:16:12 +0330

    refactor: add parameter writer and structured log state

---
+++ src/KetPsi.Extensions.Logging.Abstractions/ILogParameterWriter.cs
@@ -0,0 +1,7 @@
+﻿namespace KetPsi.Extensions.Logging.Abstractions;
+
+public interface ILogParameterWriter
+{
+    // The sink decides what to do with the Name and the strongly-typed Value
+    void Write<T>(in LogParameter<T> parameter);
+}
---
+++ src/KetPsi.Extensions.Logging.Abstractions/IStructuredLogState.cs
@@ -0,0 +1,7 @@
+﻿namespace KetPsi.Extensions.Logging.Abstractions;
+
+public interface IStructuredLogState
+{
+    ReadOnlySpan<char> OriginalFormat { get; }
+    void WriteParameters<TWriter>(ref TWriter writer) where TWriter : struct, ILogParameterWriter;
+}
